a6ddaa54f0eae536f39d8dd5a6465a3eabc8b159
VoidLock <37760961+VoidLock@users.noreply.github.com>
2025-09-03 17:56:47 -0500

    Refactor config flow to use CONF_HOST only

--- custom_components/pifire/config_flow.py
+++ custom_components/pifire/config_flow.py
@@ -1,17 +1,10 @@
 from __future__ import annotations
-
 import voluptuous as vol
 from homeassistant import config_entries
 from homeassistant.data_entry_flow import FlowResult
+from .const import DOMAIN, CONF_HOST
 
-from .const import DOMAIN, CONF_BASE_URL
-
-
-DATA_SCHEMA = vol.Schema(
-    {
-        vol.Optional(CONF_BASE_URL, default="http://pifire.local:8080"): str,
-    }
-)
+DATA_SCHEMA = vol.Schema({vol.Required(CONF_HOST, default="pifire.local"): str})
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -19,18 +12,9 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
 
     async def async_step_user(self, user_input: dict | None = None) -> FlowResult:
         if user_input is not None:
-            unique_id = (user_input.get(CONF_BASE_URL) or "").rstrip("/") or "pifire_local"
-            await self.async_set_unique_id(unique_id)
+            host = user_input[CONF_HOST].strip()
+            unique = f"http://{host}"
+            await self.async_set_unique_id(unique)
             self._abort_if_unique_id_configured()
-            return self.async_create_entry(title="PiFire", data=user_input)
-
+            return self.async_create_entry(title="PiFire", data={CONF_HOST: host})
         return self.async_show_form(step_id="user", data_schema=DATA_SCHEMA)
-
-    async def async_step_zeroconf(self, discovery_info: dict) -> FlowResult:
-        host = discovery_info.get("host")
-        port = discovery_info.get("port")
-        base_url = f"http://{host}:{port}"
-
-        await self.async_set_unique_id(base_url)
-        self._abort_if_unique_id_configured()
-        return self.async_create_entry(title="PiFire", data={CONF_BASE_URL: base_url})
